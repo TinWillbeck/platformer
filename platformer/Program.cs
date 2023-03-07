1c9fd5b3a0c9c3ade2a604386da1eca89d079c59
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2023-03-07 11:08:50 +0100

    struktur och rörelse

---
+++ platformer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+Raylib.InitWindow(1500,800, "platformer");
+Raylib.SetTargetFPS(60);
+
+Rectangle Player = new Rectangle(60,60, 60,60);
+
+List<Rectangle> structure = new();
+
+
+float speed = 10;
+float gravity = 5;
+
+
+while(Raylib.WindowShouldClose()==false)
+{
+
+    Player.y += gravity;
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)){
+        Player.y -= speed;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)){
+        Player.x -= speed;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S)){
+        Player.y += speed;
+    }
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)){
+        Player.x += speed;
+    }
+
+    structure.Add(new Rectangle(200,200,200,200));
+
+
+    
+
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLACK);
+    Raylib.DrawRectangleRec(Player, Color.WHITE);
+    Raylib.DrawRectangleRec(structure[0], Color.RED);
+    Raylib.EndDrawing();
+}
