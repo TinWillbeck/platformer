dc899a6f2e06fbf5078a16125295fe3173700683
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2023-03-10 10:00:58 +0100

    fix kanter, rensade onödig kod

--- platformer/Program.cs
+++ platformer/Program.cs
@@ -10,12 +10,12 @@ bool isTouching = false;
 Level1 level = new();
 
 
-
 string currentScene = "level1";
 
 float speed = 10;
 float jump = 120;
 float gravity = 5;
+
 
 
 while(Raylib.WindowShouldClose()==false)
@@ -24,6 +24,7 @@ while(Raylib.WindowShouldClose()==false)
     // Grafik
 
     Raylib.BeginDrawing();
+
     if (currentScene == "start"){
         Raylib.DrawText("Press ENTER to start", 550,300,32, Color.WHITE);
     }
@@ -44,26 +45,47 @@ while(Raylib.WindowShouldClose()==false)
             currentScene = "level1";
         }
     }
-    else{
+    else
+    {
 
         Player.y += gravity;
 
-        if (!Raylib.CheckCollisionRecs(Player, level.structure[0]))
-        {
-            isTouching = false;
-        }
+        isTouching = false;
         for (var i = 0; i < level.structure.Count; i++)
         {
             if (Raylib.CheckCollisionRecs(Player, level.structure[i]))
             {
-                Player.y = level.structure[i].y-Player.height;
+                Player.y = level.structure[i].y - Player.height;
                 isTouching = true;
             }
-            
-        }
+            if (Raylib.CheckCollisionRecs(Player, level.teleport[0]))
+            {
+                // currentScene = 
+            }
         }
 
+        Player = Movement(Player, isTouching, speed, jump);
+    }
 
+}
+
+static void Level(List<Rectangle> structure, List<Rectangle> teleport)
+{
+    
+    Raylib.ClearBackground(Color.BLACK);
+    Raylib.DrawText("Använd W, A, D för att flytta den vita kuben. Ta dig till gröna kuben för att gå till nästa nivå.", 100,100,26, Color.WHITE);
+    for (var i = 0; i < structure.Count; i++)
+    {
+        Raylib.DrawRectangleRec(structure[i], Color.RED);
+    }
+    for (var i = 0; i < teleport.Count; i++)
+    {
+        Raylib.DrawRectangleRec(teleport[i], Color.GREEN);
+    }
+}
+
+static Rectangle Movement(Rectangle Player, bool isTouching, float speed, float jump)
+{
     if (Raylib.IsKeyPressed(KeyboardKey.KEY_W) && isTouching == true)
     {
         Player.y -= jump;
@@ -77,20 +99,14 @@ while(Raylib.WindowShouldClose()==false)
         Player.x += speed;
     }
 
-}
+    if (Player.x <= -10)
+    {
+        Player.x += speed;
+    }
+    if (Player.x >= 1450)
+    {
+        Player.x -= speed;
+    }
 
-static void Level(List<Rectangle> structure, List<Rectangle> teleport)
-{
-    
-    Raylib.ClearBackground(Color.BLACK);
-    Raylib.DrawText("Använd W, A, D för att flytta den vita kuben. Ta dig till gröna kuben för att gå till nästa nivå.", 100,100,24, Color.WHITE);
-    for (var i = 0; i < structure.Count; i++)
-    {
-        Raylib.DrawRectangleRec(structure[i], Color.RED);
-    }
-    for (var i = 0; i < teleport.Count; i++)
-    {
-        Raylib.DrawRectangleRec(teleport[i], Color.GREEN);
-        
-    }
+    return Player;
 }
