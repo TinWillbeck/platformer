e8f00b6492c44699e85facd092adf1f4e22d5c27
Tin Willbeck <tin.willbeck@gmail.com>
2023-03-11 21:16:39 +0100

    scene switch

--- platformer/Program.cs
+++ platformer/Program.cs
@@ -10,8 +10,8 @@ bool isTouching = false;
 Level1 level1 = new();
 Level2 level2 = new();
 
-string currentScene = "level2";
-bool nextScene = false;
+string currentScene = "start";
+string nextScene = "";
 
 float speed = 10;
 float jump = 120;
@@ -22,35 +22,38 @@ float gravity = 5;
 while(Raylib.WindowShouldClose()==false)
 {
 
-    // Grafik
+    // Level init
     
     Raylib.BeginDrawing();
 
     if (currentScene == "start"){
         Raylib.DrawText("Press ENTER to start", 550,300,32, Color.WHITE);
     }
-
+    else if (currentScene == "endScreen"){
+        Raylib.DrawText("GG hoppas jag får godkänt!", 550,300,32,Color.WHITE);
+    }
     else if(currentScene == "level1")
     {
         Raylib.DrawRectangleRec(Player, Color.WHITE);
         Raylib.DrawText("Använd W, A, D för att flytta den vita kuben. Ta dig till gröna kuben för att gå till nästa nivå.", 100, 100, 26, Color.WHITE);
 
-        collisionFloor(ref Player, ref isTouching, level1.structure, level1.teleport);
+        collisionFloor(ref Player, ref isTouching, level1.structure);
         Level(level1.structure, level1.teleport, level1.wall);
-
-    
+        sceneSwitch(ref Player, level1.teleport, ref currentScene, "level2");
     }
 
     else if (currentScene == "level2")
     {
         Raylib.DrawRectangleRec(Player, Color.WHITE);
 
-        collisionFloor(ref Player, ref isTouching, level2.structure, level2.teleport);
-
+        collisionFloor(ref Player, ref isTouching, level2.structure);
         Level(level2.structure, level2.teleport, level2.wall);
-
         collisionWall(ref Player, ref isTouching, level2.wall, speed, gravity);
+        sceneSwitch(ref Player, level2.teleport, ref currentScene, "endScreen");
+
     }
+
+    
 
 
     Raylib.EndDrawing();
@@ -71,6 +74,8 @@ while(Raylib.WindowShouldClose()==false)
     }
 
 }
+
+// metoder
 
 static void Level(List<Rectangle> structure, List<Rectangle> teleport, List<Rectangle> wall)
 {
@@ -117,7 +122,7 @@ static Rectangle Movement(Rectangle Player, bool isTouching, float speed, float 
     return Player;
 }
 
-static void collisionFloor(ref Rectangle Player, ref bool isTouching, List<Rectangle> structure, List<Rectangle> teleport)
+static void collisionFloor(ref Rectangle Player, ref bool isTouching, List<Rectangle> structure)
 {
     isTouching = false;
     for (var i = 0; i < structure.Count; i++)
@@ -127,11 +132,6 @@ static void collisionFloor(ref Rectangle Player, ref bool isTouching, List<Recta
         {
             Player.y = structure[i].y - Player.height;
             isTouching = true;
-        }
-        if (Raylib.CheckCollisionRecs(Player, teleport[0]))
-        {
-            Player.x = 60;
-            Player.y = 660;
         }
 
     }
@@ -155,5 +155,15 @@ static void collisionWall(ref Rectangle Player, ref bool isTouching, List<Rectan
                 Player.x -= speed;
             }
         }
+    }
+}
+
+static void sceneSwitch(ref Rectangle Player, List<Rectangle> teleport, ref string currentScene, string nextScene)
+{
+    if (Raylib.CheckCollisionRecs(Player, teleport[0]))
+    {
+        Player.x = 60;
+        Player.y = 660;
+        currentScene = nextScene;
     }
 }
