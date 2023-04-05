b6e23dbde7a230797f5fce0684852e7506c787e5
Tin Willbeck <tin.willbeck@gmail.com>
2023-04-05 21:03:43 +0200

    lvl4 forts

--- platformer/level4.cs
+++ platformer/level4.cs
@@ -26,12 +26,20 @@ public class Level4
         structure.Add(new Rectangle(1300, 200, 100,10));
         structure.Add(new Rectangle(900, 600, 100,10));
         structure.Add(new Rectangle(600, 500, 100,10));
+        structure.Add(new Rectangle(300, 350, 100,10));
+        structure.Add(new Rectangle(600, 250, 100,10));
         // Tak
-
+        roof.Add(new Rectangle(300,240,100,10));
+        roof.Add(new Rectangle(900,610,100,10));
+        roof.Add(new Rectangle(600,510,100,10));
+        roof.Add(new Rectangle(600,170,100,10));
         // Väggar
 
         // Block
         block.Add(new Rectangle(1300,210,100,490));
+        block.Add(new Rectangle(300,360,100,100));
+        block.Add(new Rectangle(300,0,100,240));
+        block.Add(new Rectangle(600,260,100,100));
         // Killfloor (Golv som TPar spelaren till början av leveln)
          killFloor.Add(new Rectangle(200,750,600,100));       
         // Teleport
