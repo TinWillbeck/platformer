4ec57e1889cc3958d8692c711e6a2b27ca4f5bb3
Tin Willbeck <tin.willbeck@elev.ga.ntig.se>
2023-04-20 16:03:31 +0200

    påbörjad enemy klass

---
+++ platformer/enemy.cs
@@ -0,0 +1,21 @@
+using System;
+using Raylib_cs;
+
+// Ladda in enemy, individuell enemy kan skapas i level klass på något vis
+// X och Y max samt min värde
+// Rörelsehastighet
+// Gör så den rör sig mellan max och min koordinater, till en början bara höger och vänster
+// Kolla kollision med spelare
+// Flytta spelare till spawn om de kolliderar
+
+
+
+public class EnemyClass
+{
+    public Rectangle Enemy(Rectangle enemy, float maxX, float maxY, float enemySpeed, Rectangle player)
+    {
+        
+
+        return player;
+    }
+}
